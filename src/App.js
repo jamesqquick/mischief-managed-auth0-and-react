45fad453edca637c147558b55a3d93b3e7714005
James Quick <james.q.quick@outlook.com>
2019-11-16 08:42:43 -0600

    Resetting app.js to before auth0 stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
-import { useAuth0 } from './react-auth0-spa';
 
 function App() {
-    const { loading, loginWithRedirect, logout, isAuthenticated } = useAuth0();
-
     return (
         <div className="App">
             <h1>Mischief Managed....Not Yet!</h1>
-            {isAuthenticated ? (
-                <>
-                    <button onClick={logout}>Mischief Managed</button>
-                    <Link to="/mischief">Search For Mischief!</Link>
-                </>
-            ) : (
-                <button onClick={loginWithRedirect}>
-                    I Solemly Swear I am Up To No Good
-                </button>
-            )}
+            <button>Mischief Managed</button>
+            <Link to="/mischief">Search For Mischief!</Link>
+            <button>I Solemly Swear I am Up To No Good</button>
         </div>
     );
 }
