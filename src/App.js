70b3ee13e9ca4ab527f204d22aa24767da6f9fcc
James Quick <james.q.quick@outlook.com>
2019-11-16 08:22:28 -0600

    Removed logo and updated app component with login, logout buttons and a link to the mischief page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Mischief Managed....Not Yet!</h1>
+            <button>I Solemly Swear I am Up To No Good</button>
+            <button>Mischief Managed</button>
+            <Link to="/mischief">Search For Mischief!</Link>
+        </div>
+    );
 }
 
 export default App;
