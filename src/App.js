4a30c855ac967a77fc3b46469e123b30cdcb0f2c
James Quick <james.q.quick@outlook.com>
2019-11-17 10:46:34 -0600

    Added auth0 code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
-
+import { useAuth0 } from './react-auth0-spa';
 function App() {
+    const { loading, loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <h1>Mischief Managed</h1>
             <h2 className="fade-in">...OR IS IT?!?!?!</h2>
-            <button className="btn">Mischief Managed</button>
-            <button className="btn">I Solemly Swear I am Up To No Good</button>
-            <Link to="/mischief">Search For Mischief!</Link>
+            {!isAuthenticated && (
+                <button className="btn" onClick={loginWithRedirect}>
+                    I Solemly Swear I am Up To No Good
+                </button>
+            )}
+            {isAuthenticated && (
+                <>
+                    <button className="btn" onClick={logout}>
+                        Mischief Managed
+                    </button>
+                    <Link to="/mischief">Search For Mischief!</Link>
+                </>
+            )}
         </div>
     );
 }
